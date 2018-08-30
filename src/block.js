ce072ea63bd7df898184b856496355000b2d4684
Milan Jaritz <milan.jaritz@gmail.com>
2018-08-30 20:34:38 +0200

    update

--- src/block.js
+++ src/block.js
@@ -1,9 +1,13 @@
 
 class Block {
 
-    constructor() {
-        this.type = int(random(BLOCK_TYPES.length));
+    constructor(type, pos) {
+        switch(type) {
+            case RANDOM: this.type = int(random(BLOCK_TYPES.length)); break;
+            default    : this.type = type;                            break;
+        }
 
+        this.pos = pos;
         this.dead = false;
 
         // [ 00  10  20  30 ]
@@ -12,45 +16,71 @@ class Block {
         // [ 03  13  23  33 ]
 
         switch(this.type) {
-            case I: this.cubes = [ { x: 1,  y: 0 }, { x: 1,  y: 1 }, { x: 1,  y: 2 }, { x: 1, y: 3 } ]; // [1]
+            case I: this.cubes = [ { x: 0,  y: 0 }, { x: 1,  y: 0 }, { x: 2,  y: 0 }, { x: 3, y: 0 } ];
                     this.color = color("#00F0F0");
-                    this.anchor = 1;
+                    this.anchor = 2;
+                    this.size = { w: 4, h: 1 };
                     break;
-            case J: this.cubes = [ { x: 2,  y: 0 }, { x: 2,  y: 1 }, { x: 1,  y: 2 }, { x: 2, y: 2 } ]; // [1]
+            case J: this.cubes = [ { x: 0,  y: 0 }, { x: 0,  y: 1 }, { x: 1,  y: 1 }, { x: 2, y: 1 } ];
                     this.color = color("#0000F0");
-                    this.anchor = 1;
+                    this.anchor = 3;
+                    this.size = { w: 3, h: 2 };
                     break;
-            case L: this.cubes = [ { x: 1,  y: 0 }, { x: 1,  y: 1 }, { x: 1,  y: 2 }, { x: 2, y: 2 } ]; // [1]
+            case L: this.cubes = [ { x: 0,  y: 1 }, { x: 1,  y: 1 }, { x: 2,  y: 1 }, { x: 2, y: 0 } ];
                     this.color = color("#F0A000");
-                    this.anchor = 1;
+                    this.anchor = 2;
+                    this.size = { w: 3, h: 2 };
                     break;
-            case O: this.cubes = [ { x: 1,  y: 0 }, { x: 2,  y: 0 }, { x: 1,  y: 1 }, { x: 2, y: 1 } ]; // X
+            case O: this.cubes = [ { x: 1,  y: 0 }, { x: 2,  y: 0 }, { x: 1,  y: 1 }, { x: 2, y: 1 } ];
                     this.color = color("#F0F000");
                     this.anchor = -1;
+                    this.size = { w: 4, h: 2 };
                     break;
-            case S: this.cubes = [ { x: 1,  y: 0 }, { x: 2,  y: 0 }, { x: 0,  y: 1 }, { x: 1, y: 1 } ]; // [0]
+            case S: this.cubes = [ { x: 1,  y: 0 }, { x: 2,  y: 0 }, { x: 0,  y: 1 }, { x: 1, y: 1 } ];
                     this.color = color("#00F000");
-                    this.anchor = 0;
+                    this.anchor = 3;
+                    this.size = { w: 3, h: 2 };
                     break;
-            case T: this.cubes = [ { x: 0,  y: 0 }, { x: 1,  y: 0 }, { x: 2,  y: 0 }, { x: 1, y: 1 } ]; // [1]
+            case T: this.cubes = [ { x: 1,  y: 0 }, { x: 0,  y: 1 }, { x: 1,  y: 1 }, { x: 2, y: 1 } ];
                     this.color = color("#A000F0");
-                    this.anchor = 1;
+                    this.anchor = 2;
+                    this.size = { w: 3, h: 2 };
                     break;
-            case Z: this.cubes = [ { x: 0,  y: 0 }, { x: 1,  y: 0 }, { x: 1,  y: 1 }, { x: 2, y: 1 } ]; // [1]
+            case Z: this.cubes = [ { x: 0,  y: 0 }, { x: 1,  y: 0 }, { x: 1,  y: 1 }, { x: 2, y: 1 } ];
                     this.color = color("#F00000");
-                    this.anchor = 1;
+                    this.anchor = 2;
+                    this.size = { w: 3, h: 2 };
                     break;
         }
 
-        for(let c of this.cubes)
-            c.x += int(GRID.x / 2) - 2;
+        let distance = 0;
+        switch(pos) {
+            case LEFT  : distance = 0.0; break;
+            case CENTER: distance = 0.5; break;
+            case RIGHT : distance = 0.0; break;
+        }
+
+        for(let c of this.cubes) {
+            c.x += int((GRID.x - 1) * distance);
+        }
 
     }
 
-    draw() {
+    draw(y) {
+        stroke(0);
         fill(this.color);
         for(let c of this.cubes) {
-            rect(c.x * BLOCK_SIZE, c.y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+            switch(this.pos) {
+                case LEFT:
+                    rect(c.x * BLOCK_SIZE_SMALL + 10 + (HUD_SIZE - 20) / 2 - this.size.w * BLOCK_SIZE_SMALL / 2, c.y * BLOCK_SIZE_SMALL + 10 + (HUD_SIZE - 20) / 2 - this.size.h * BLOCK_SIZE_SMALL / 2, BLOCK_SIZE_SMALL, BLOCK_SIZE_SMALL);
+                    break;
+                case CENTER:
+                    rect(c.x * BLOCK_SIZE + HUD_SIZE, c.y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+                    break;
+                case RIGHT:
+                    rect(width - 0.5 * HUD_SIZE - this.size.w * BLOCK_SIZE_SMALL / 2 + c.x * BLOCK_SIZE_SMALL, c.y * BLOCK_SIZE_SMALL + 110 * y + 80, BLOCK_SIZE_SMALL, BLOCK_SIZE_SMALL);
+                    break;
+            }
         }
     }
 
@@ -63,8 +93,13 @@ class Block {
                 return -1;
             } else if(c.y + y > GRID.y - 1) {
                 this.updateWithGrid();
+                return;
             } else if(grid[c.y + y][c.x + x] !== -1) {
+                if(x !== 0) {
+                    return;
+                }
                 this.updateWithGrid();
+                return;
             }
 
         }
@@ -77,6 +112,7 @@ class Block {
             grid[c.y][c.x] = this.color;
         }
         this.dead = true;
+        savedBlockUsed = false;
     }
 
     rotate() {
@@ -85,21 +121,16 @@ class Block {
         
         let newCubes = [];
         for(let c of this.cubes) {
-            let x = c.x - this.cubes[this.anchor].x; // translate to anchor and rotate
+            let x = c.x - this.cubes[this.anchor].x; // translate to anchor
             let y = c.y - this.cubes[this.anchor].y;
 
-            let rx = y * -1;
+            let rx = y * -1;                         // rotate
             let ry = x;
 
-            rx += this.cubes[this.anchor].x;
+            rx += this.cubes[this.anchor].x;         // translate back
             ry += this.cubes[this.anchor].y;
 
             newCubes.push({ x: rx, y: ry });
-
-
-            // let x = -1 * (c.y - this.cubes[this.anchor].y); // translate to anchor and rotate
-            // x += this.cubes[this.anchor].y;                 // translate back
-            // newCubes.push({ x: x, y: c.x });
         }
         this.cubes = newCubes;
     }
--- src/sketch.js
+++ src/sketch.js
@@ -1,25 +1,35 @@
 
-const GRID = { x: 10, y: 20 };
+const GRID = { x: 10, y: 21 };
 const BLOCK_SIZE = 30;
+const BLOCK_SIZE_SMALL = BLOCK_SIZE * 0.7;
 const I = 0, J = 1, L = 2, O = 3, S = 4, T = 5, Z = 6;
-const BLOCK_TYPES = [I, J, L, O, S, T, Z];
-const HUD_SIZE = 100;
+const BLOCK_TYPES = [ I, J, L, O, S, T, Z ];
+const HUD_SIZE = 110;
+const RANDOM = 99;
 
+var upcomingBlocks = [];
+var score = 0;
 var grid = [];
 var timer = 30;
 var animationTimer = 0;
 var keyTimers = [ 0, 0, 0 ];
+var savedBlock;
+var savedBlockUsed = false;
 
 var block;
 
 function setup() {
-    createCanvas(GRID.x * BLOCK_SIZE + HUD_SIZE, GRID.y * BLOCK_SIZE);
+    createCanvas(GRID.x * BLOCK_SIZE + HUD_SIZE * 2, GRID.y * BLOCK_SIZE);
 
     for(let a = 0; a < GRID.y; a++) {
         pushNewLineToGrid();
     }
 
-    block = new Block();
+    for(let i = 0; i < 5; i++) {
+        upcomingBlocks.push(new Block(RANDOM, RIGHT));
+    }
+
+    block = new Block(RANDOM, CENTER);
 }
 
 function draw() {
@@ -28,26 +38,49 @@ function draw() {
     // BLOCK SPAWN ------------------------------------------------------------
 
     if(block.dead) {
-        block = new Block();
+        block = new Block(getUpcomingBlock().type, CENTER);
     }
 
     // HUD --------------------------------------------------------------------
 
     fill("#333");
     noStroke();
-    rect(GRID.x * BLOCK_SIZE, 0, HUD_SIZE, height);
-
-    block.draw();
-
+    rect(0, 0, HUD_SIZE, height);
+    rect(GRID.x * BLOCK_SIZE + HUD_SIZE, 0, HUD_SIZE, height);
+    
+    fill("#111");
+    rect(10, 10, HUD_SIZE - 20, HUD_SIZE - 20);
+    
+    
+    
+    fill(255);
+    textSize(20);
+    textAlign(CENTER);
+    text("SCORE \n" + score, width - HUD_SIZE / 2, 20);
+    
+    
     // DRAW BLOCKS ------------------------------------------------------------
+    
+    // UPCOMING
+    for(let i = 0; i < upcomingBlocks.length; i++) {
+        upcomingBlocks[i].draw(i);
+    }
 
+    // SAVED BLOCK
+    if(savedBlock)
+        savedBlock.draw();
+
+    // THE BLOCK
+    block.draw();
+    
+    // THE GRID
     let rowsToDelete = [];
     for(let a = 0; a < GRID.y; a++) {
         let cubesInRow = 0;
         for(let i = 0; i < GRID.x; i++) {
             if(grid[a][i] !== -1) {
                 fill(grid[a][i]);
-                rect(i * BLOCK_SIZE, a * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+                rect(HUD_SIZE + i * BLOCK_SIZE, a * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
                 cubesInRow++;
             }
             if(cubesInRow === GRID.x) {
@@ -55,6 +88,9 @@ function draw() {
             }
         }
     }
+    // fill("#333");
+    // noStroke();
+    // rect(HUD_SIZE, 0, GRID.x * BLOCK_SIZE, BLOCK_SIZE - 10);
 
     // REMOVE BLOCKS ----------------------------------------------------------
 
@@ -62,15 +98,21 @@ function draw() {
         fill(255);
         for(let r of rowsToDelete) {
             if(animationTimer % 10 > 5) {
-                rect(0, r * BLOCK_SIZE, GRID.x * BLOCK_SIZE, BLOCK_SIZE);
+                rect(HUD_SIZE, r * BLOCK_SIZE, GRID.x * BLOCK_SIZE, BLOCK_SIZE);
             }
         }
-        if(animationTimer == 25) {
+        if(animationTimer === 25) {
             for(let i = rowsToDelete.length - 1; i >= 0; i--) {
                 grid.splice(rowsToDelete[i], 1);
             }
             for(let i = 0; i < rowsToDelete.length; i++) {
                 pushNewLineToGrid();
+            }
+            switch (rowsToDelete.length) {
+                case 1: score += 10; break;
+                case 2: score += 30; break;
+                case 3: score += 60; break;
+                case 4: score += 100; break;
             }
             animationTimer = -1;
         }
@@ -101,7 +143,7 @@ function draw() {
 
     timer--;
 
-    if(timer == 0) {
+    if(timer === 0) {
         block.modPos(0, 1);
         timer = 50;
     }
@@ -112,6 +154,25 @@ function keyPressed() {
     if(keyCode === UP_ARROW) {
         block.rotate();
     }
+    if(keyCode === 67) { // C
+        if(!savedBlockUsed) {
+            savedBlockUsed = true;
+            let save = savedBlock;
+            savedBlock = new Block(block.type, LEFT);
+            savedBlock.type = block.type;
+            if(!save) {
+                block = new Block(getUpcomingBlock().type, CENTER);
+            } else {
+                block = new Block(save.type, CENTER);
+            }
+        }
+    }
+}
+
+function getUpcomingBlock() {
+    let b = upcomingBlocks.splice(0, 1);
+    upcomingBlocks.push(new Block(RANDOM, RIGHT));
+    return b[0];
 }
 
 function pushNewLineToGrid() {
